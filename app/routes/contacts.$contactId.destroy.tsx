a072453eb535113c8ba46aa04e3f7729e85e7934
Evan Marie Carr <evanmariecarr@gmail.com>
2023-10-07 20:41:33 -0400

    completed the tutorial

---
+++ app/routes/contacts.$contactId.destroy.tsx
@@ -0,0 +1,9 @@
+import { ActionFunctionArgs, redirect } from "react-router";
+import invariant from "tiny-invariant";
+import { deleteContact } from "~/data";
+
+export const action = async ({ params }: ActionFunctionArgs) => {
+  invariant(params.contactId, "missing contactId param");
+  await deleteContact(params.contactId);
+  return redirect("/");
+};
